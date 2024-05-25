90bc9cae57b1367fd7acaf8b2b2ab1a2566d4db2
David Agredano <davidagredano@gmail.com>
2024-05-25 16:44:38 +0200

    Add function to fetch and log superheroes data

---
+++ scripts.js
@@ -0,0 +1,12 @@
+async function getSuperHeroes() {
+  const requestURL =
+    "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+  const request = new Request(requestURL);
+
+  const response = await fetch(request);
+  const superHeroes = await response.json();
+
+  console.log(superHeroes);
+}
+
+getSuperHeroes();
